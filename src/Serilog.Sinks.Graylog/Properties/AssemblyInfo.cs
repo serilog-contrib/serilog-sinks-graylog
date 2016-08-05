b8e52098256ceee2cbf246d573cc22d594c7cff4
whirlwind <whirlwind432@gmail.com>
2016-08-05 12:53:24 +0300

    fix assebly ver

--- src/Serilog.Sinks.Graylog/Properties/AssemblyInfo.cs
+++ src/Serilog.Sinks.Graylog/Properties/AssemblyInfo.cs
@@ -6,14 +6,14 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Serilog sync for Graylog")]
-[assembly: AssemblyDescription("The Serilog Graylog sink project is a sink (basically a writer) for the Serilog logging framework. Structured log events are written to sinks and each sink is responsible for writing it to its own backend, database, store etc. This sink delivers the data to Graylog2, a NoSQL search engine.")]
+[assembly: AssemblyDescription("The Serilog Graylog Sink project is a sink (basically a writer) for the Serilog logging framework. Structured log events are written to sinks and each sink is responsible for writing it to its own backend, database, store etc. This sink delivers the data to Graylog2, a NoSQL search engine.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Serilog.Sinks.Graylog")]
-[assembly: AssemblyCopyright("Copyright © whirlwind432@gmail.com 2016")]
+[assembly: AssemblyCopyright("whirlwind432@gmail.com Copyright © 2016")]
 [assembly: AssemblyCompany("Anton Volkov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
+[assembly: AssemblyInformationalVersion("1.0.0-master")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0")]
+
