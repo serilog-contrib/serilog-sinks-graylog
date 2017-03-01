3090a976d58ae6f86e1851141ed8c9dd415aca73
whirlwind <whirlwind432@gmail.com>
2017-03-01 16:04:16 +0300

    Revert "Pack nuget on build"
    This reverts commit 272eb869b9ec5c6d2226283a80c48511381ca581.

---
+++ src/Serilog.Sinks.Graylog/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Serilog sync for Graylog")]
+[assembly: AssemblyDescription("The Serilog Graylog Sink project is a sink (basically a writer) for the Serilog logging framework. Structured log events are written to sinks and each sink is responsible for writing it to its own backend, database, store etc. This sink delivers the data to Graylog2, a NoSQL search engine.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Serilog.Sinks.Graylog")]
+[assembly: AssemblyCopyright("whirlwind432@gmail.com Copyright © 2016")]
+[assembly: AssemblyCompany("Anton Volkov")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyInformationalVersion("1.0.0-master")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3466c882-d569-4ca5-8db6-0ea53c5a5c57")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+
