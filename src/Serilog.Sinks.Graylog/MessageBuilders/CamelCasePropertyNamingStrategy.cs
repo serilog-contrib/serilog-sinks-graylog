e19d70a4c048361710d8d59146cb2e901ab84594
Jp Dienst <jpdienst@live.com>
2017-10-20 18:34:05 -0400

    Add property naming strategy for normalization.

---
+++ src/Serilog.Sinks.Graylog/MessageBuilders/CamelCasePropertyNamingStrategy.cs
@@ -0,0 +1,41 @@
+﻿namespace Serilog.Sinks.Graylog.MessageBuilders
+{
+    public class CamelCasePropertyNamingStrategy : IPropertyNamingStrategy
+    {
+        public string GetPropertyName(string property)
+        {
+            var camelCased = ToCamelCase(property);
+            return camelCased;
+        }
+
+        private static string ToCamelCase(string s)
+        {
+            if (string.IsNullOrEmpty(s) || !char.IsUpper(s[0]))
+            {
+                return s;
+            }
+
+            char[] chars = s.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (i == 1 && !char.IsUpper(chars[i]))
+                {
+                    break;
+                }
+
+                bool hasNext = (i + 1 < chars.Length);
+                if (i > 0 && hasNext && !char.IsUpper(chars[i + 1]))
+                {
+                    break;
+                }
+
+                char c;
+                c = char.ToLowerInvariant(chars[i]);
+                chars[i] = c;
+            }
+
+            return new string(chars);
+        }
+    }
+}
---
+++ src/Serilog.Sinks.Graylog/MessageBuilders/NoOpPropertyNamingStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace Serilog.Sinks.Graylog.MessageBuilders
+{
+    public class NoOpPropertyNamingStrategy : IPropertyNamingStrategy
+    {
+        public string GetPropertyName(string property)
+        {
+            return property;
+        }
+    }
+}
