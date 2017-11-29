serilog-contrib/serilog-sinks-graylog

commit ec3c9b029ca84e3d1c06fc3c2e810d325c4b759b

refactoring

src/Serilog.Sinks.Graylog/Transport/Udp/UdpTransportClient.cs
@@ -25,11 +25,11 @@ namespace Serilog.Sinks.Graylog.Transport.Udp
         /// Sends the specified payload.
         /// </summary>
         /// <param name="payload">The payload.</param>
-        public async Task Send(byte[] payload)
+        public Task Send(byte[] payload)
         {
             using (var udpClient = new UdpClient())
             {
-                await udpClient.SendAsync(payload, payload.Length, _target);
+                return udpClient.SendAsync(payload, payload.Length, _target);
             }
         }
     }

src/Serilog.Sinks.Graylog.Tests/Extensions/StringExtensionsFixture.cs
@@ -25,7 +25,7 @@ namespace Serilog.Sinks.Graylog.Tests.Extensions
         [InlineData("SomeTestString", "SomeTestString", 200)]
         public void WhenShortMessage_ThenResultShouldBeExpected(string given, string expected, int length)
         {
-            var actual = given.ShortMessage(length);
+            var actual = given.Truncate(length);
 
             actual.ShouldBeEquivalentTo(expected);
         }

src/Serilog.Sinks.Graylog.Tests/Helpers/MessageIdGeneratorFixture.cs
@@ -21,12 +21,12 @@ namespace Serilog.Sinks.Graylog.Tests.Helpers
         public void WhenGenerateFromTimeStamp_ThenReturnsExpectedResult()
         {
             DateTime time = DateTime.Now;
-
-            var target = new TimestampMessageIdGenerator(time);
+            byte[] given = _fixture.CreateMany<byte>(10).ToArray();
+            var target = new TimestampMessageIdGenerator();
 
             byte[] expected = BitConverter.GetBytes(time.Ticks);
 
-            byte[] actual = target.GenerateMessageId();
+            byte[] actual = target.GenerateMessageId(given);
 
             actual.ShouldBeEquivalentTo(expected);
         }
@@ -36,12 +36,12 @@ namespace Serilog.Sinks.Graylog.Tests.Helpers
         {
             byte[] given = _fixture.CreateMany<byte>(10).ToArray();
 
-            var target = new Md5MessageIdGenerator(given);
+            var target = new Md5MessageIdGenerator();
 
             MD5 md5 = MD5.Create();
             var expected = md5.ComputeHash(given).Take(8).ToArray();
 
-            var actual = target.GenerateMessageId();
+            var actual = target.GenerateMessageId(given);
 
             actual.ShouldBeEquivalentTo(expected);
         }
@@ -51,7 +51,7 @@ namespace Serilog.Sinks.Graylog.Tests.Helpers
         {
             var resolver = new MessageIdGeneratorResolver();
 
-            IMessageIdGenerator actual = resolver.Resolve(MessageIdGeneratortype.Md5, null);
+            IMessageIdGenerator actual = resolver.Resolve(MessageIdGeneratortype.Md5);
 
             Assert.IsType<Md5MessageIdGenerator>(actual);
         }
@@ -61,7 +61,7 @@ namespace Serilog.Sinks.Graylog.Tests.Helpers
         {
             var resolver = new MessageIdGeneratorResolver();
 
-            IMessageIdGenerator actual = resolver.Resolve(MessageIdGeneratortype.Timestamp, null);
+            IMessageIdGenerator actual = resolver.Resolve(MessageIdGeneratortype.Timestamp);
 
             Assert.IsType<TimestampMessageIdGenerator>(actual);
         }

src/Serilog.Sinks.Graylog.Tests/Transport/Udp/DataToChunkConverterFixture.cs
@@ -62,9 +62,9 @@ namespace Serilog.Sinks.Graylog.Tests.Transport.Udp
 
             var messageId = _fixture.CreateMany<byte>(8).ToArray();
 
-            idGenerator.Setup(c => c.GenerateMessageId()).Returns(messageId);
+            idGenerator.Setup(c => c.GenerateMessageId(giwenData)).Returns(messageId);
 
-            _resolver.Setup(c => c.Resolve(_settings.MessageIdGeneratorType, It.IsAny<byte[]>()))
+            _resolver.Setup(c => c.Resolve(_settings.MessageIdGeneratorType))
                 .Returns(idGenerator.Object);
 
             var target = new DataToChunkConverter(_settings, _resolver.Object);

src/Serilog.Sinks.Graylog/Extensions/DateTimeExtensions.cs
@@ -1,0 +1,21 @@
+﻿using System;
+
+namespace Serilog.Sinks.Graylog.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        /// <summary>
+        /// Converts to nix date time.
+        /// </summary>
+        /// <param name="dateTime">The date time.</param>
+        /// <returns></returns>
+        public static double ConvertToNix(this DateTime dateTime)
+        {
+            var duration = dateTime.ToUniversalTime() - Epoch;
+            return Math.Round(duration.TotalSeconds, 3, MidpointRounding.AwayFromZero);
+        }
+
+    }
+}

src/Serilog.Sinks.Graylog/Extensions/StringExtensions.cs
@@ -17,7 +17,13 @@ namespace Serilog.Sinks.Graylog.Extensions
             return resultStream.ToArray();
         }
 
-        public static string ShortMessage(this string source, int maxLength)
+        /// <summary>
+        /// Truncates the specified maximum length.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="maxLength">The maximum length.</param>
+        /// <returns></returns>
+        public static string Truncate(this string source, int maxLength)
         {
             return source.Length > maxLength 
                 ? source.Substring(0, maxLength) 

src/Serilog.Sinks.Graylog/GelfConverter.cs
@@ -13,7 +13,7 @@ namespace Serilog.Sinks.Graylog
 
     public class GelfConverter : IGelfConverter
     {
-        private IDictionary<BuilderType, Lazy<IMessageBuilder>> _messageBuilders;
+        private readonly IDictionary<BuilderType, Lazy<IMessageBuilder>> _messageBuilders;
 
         public GelfConverter(IDictionary<BuilderType, Lazy<IMessageBuilder>> messageBuilders)
         {

src/Serilog.Sinks.Graylog/GraylogSink.cs
@@ -70,7 +70,7 @@ namespace Serilog.Sinks.Graylog
         {
             JObject json = _converter.GetGelfJson(logEvent);
 
-            Task.Factory.StartNew(() => _transport.Send(json.ToString(Newtonsoft.Json.Formatting.None))).GetAwaiter().GetResult();
+            Task.Factory.StartNew(async () => await _transport.Send(json.ToString(Newtonsoft.Json.Formatting.None)).ConfigureAwait(false)).GetAwaiter().GetResult();
         }
     }
 }

commit 13f761cda653b10175ed08005703f0b235c2b341

Fixing more issues introduced from ec3c9b029ca84e3d1c06fc3c2e810d325c4b759b refactor

src/Serilog.Sinks.Graylog/Transport/Udp/UdpTransport.cs
@@ -28,13 +28,14 @@ namespace Serilog.Sinks.Graylog.Transport.Udp
         /// <param name="message">The message.</param>
         /// <exception cref="System.ArgumentException">message was too long</exception>
         /// <exception cref="ArgumentException">message was too long</exception>
-        public Task Send(string message)
+        public async Task Send(string message)
         {
             byte[] compressedMessage = message.Compress();
             IList<byte[]> chunks = _chunkConverter.ConvertToChunks(compressedMessage);
-
-            var sendTasks = chunks.Select(c => _transportClient.Send(c));
-            return Task.WhenAll(sendTasks.ToArray());
+            foreach (var chunk in chunks)
+            {
+                await _transportClient.Send(chunk);
+            }
         }
     }
 }

src/Serilog.Sinks.Graylog/Transport/Udp/UdpTransportClient.cs
@@ -25,11 +25,11 @@ namespace Serilog.Sinks.Graylog.Transport.Udp
         /// Sends the specified payload.
         /// </summary>
         /// <param name="payload">The payload.</param>
-        public Task Send(byte[] payload)
+        public async Task Send(byte[] payload)
         {
             using (var udpClient = new UdpClient())
             {
-                return udpClient.SendAsync(payload, payload.Length, _target);
+                await udpClient.SendAsync(payload, payload.Length, _target);
             }
         }
     }