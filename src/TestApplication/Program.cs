6d8d1eed5fe0c7518bbb637fd89dda3d89c3b9b4
Anton Volkov <whirlwind432@gmail.com>
2019-09-04 09:42:28 +0300

    add test application

---
+++ src/TestApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Core;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                                .AddJsonFile("appsettings.json")
+                                .Build();
+            
+            Logger logger = new LoggerConfiguration()
+                            .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            logger.Warning("some warning: {test}", "test message");
+
+
+            await Task.Delay(TimeSpan.FromMinutes(1));
+        }
+    }
+}
